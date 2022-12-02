c5ad732fe4d89b134b49f6e20b050e1d68771202
Durba Chatterjee <durba.chatterjee94@gmail.com>
2022-12-03 01:50:36 +0530

    Update: Added iPUF and its variants

--- test_run.sh
+++ test_run.sh
@@ -24,3 +24,39 @@ yacc -d puf_g.y
 cc -c lex.yy.c y.tab.c
 cc -o puf_g.out lex.yy.o y.tab.o -ll
  ./puf_g.out puf_designs/test_ropuf.pufg 
+
+echo "---------------------------------------------------------------" 
+echo "                           IPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g_flatten.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_ipuf.pufg 
+
+echo "---------------------------------------------------------------"  # ne but no sample complexity expression
+echo "                           DOMINO-IPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g_flatten.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_domino_ipuf.pufg 
+
+echo "---------------------------------------------------------------"   # ne but no sample complexity expression
+echo "                           XOR-DOMINO-IPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g_flatten.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_xor_domino_ipuf.pufg 
+
+echo "---------------------------------------------------------------" 
+echo "                           XOR-IPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g_flatten.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_xor_ipuf.pufg 
