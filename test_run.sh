e104f36cf36a44474c524b9d21783a6637c25976
Durba Chatterjee <durba.chatterjee94@gmail.com>
2022-12-04 04:31:07 +0530

    Updated run script

--- test_run.sh
+++ test_run.sh
@@ -1,65 +1,65 @@
-# echo "---------------------------------------------------------------" 
-# echo "                           APUF" 
-# echo "---------------------------------------------------------------" 
-# lex puf_g.l
-# yacc -d puf_g.y
-# cc -c lex.yy.c y.tab.c
-# cc -o puf_g.out lex.yy.o y.tab.o -ll
-#  ./puf_g.out puf_designs/test_apuf.pufg 
+echo "---------------------------------------------------------------" 
+echo "                           APUF" 
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_apuf.pufg 
 
-# echo "---------------------------------------------------------------" # 
-# echo "                           XORAPUF"
-# echo "---------------------------------------------------------------" 
-# lex puf_g.l
-# yacc -d puf_g.y
-# cc -c lex.yy.c y.tab.c
-# cc -o puf_g.out lex.yy.o y.tab.o -ll
-#  ./puf_g.out puf_designs/test_xorapuf.pufg 
+echo "---------------------------------------------------------------" # 
+echo "                           XORAPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_xorapuf.pufg 
 
-# echo "---------------------------------------------------------------" 
-# echo "                           ROPUF"
-# echo "---------------------------------------------------------------" 
-# lex puf_g.l
-# yacc -d puf_g.y
-# cc -c lex.yy.c y.tab.c
-# cc -o puf_g.out lex.yy.o y.tab.o -ll
-#  ./puf_g.out puf_designs/test_ropuf.pufg 
+echo "---------------------------------------------------------------" 
+echo "                           ROPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_ropuf.pufg 
 
-# echo "---------------------------------------------------------------" 
-# echo "                           IPUF"
-# echo "---------------------------------------------------------------" 
-# lex puf_g.l
-# yacc -d puf_g.y
-# cc -c lex.yy.c y.tab.c
-# cc -o puf_g.out lex.yy.o y.tab.o -ll
-#  ./puf_g.out puf_designs/test_ipuf.pufg 
+echo "---------------------------------------------------------------" 
+echo "                           IPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_ipuf.pufg 
 
-# echo "---------------------------------------------------------------"  # ne but no sample complexity expression
-# echo "                           DOMINO-IPUF"
-# echo "---------------------------------------------------------------" 
-# lex puf_g.l
-# yacc -d puf_g.y
-# cc -c lex.yy.c y.tab.c
-# cc -o puf_g.out lex.yy.o y.tab.o -ll
-#  ./puf_g.out puf_designs/test_domino_ipuf.pufg 
+echo "---------------------------------------------------------------"  # ne but no sample complexity expression
+echo "                           DOMINO-IPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_domino_ipuf.pufg 
 
-# echo "---------------------------------------------------------------"   # ne but no sample complexity expression
-# echo "                           XOR-DOMINO-IPUF"
-# echo "---------------------------------------------------------------" 
-# lex puf_g.l
-# yacc -d puf_g.y
-# cc -c lex.yy.c y.tab.c
-# cc -o puf_g.out lex.yy.o y.tab.o -ll
-#  ./puf_g.out puf_designs/test_xor_domino_ipuf.pufg 
+echo "---------------------------------------------------------------"   # ne but no sample complexity expression
+echo "                           XOR-DOMINO-IPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_xor_domino_ipuf.pufg 
 
-# echo "---------------------------------------------------------------" 
-# echo "                           XOR-IPUF"
-# echo "---------------------------------------------------------------" 
-# lex puf_g.l
-# yacc -d puf_g.y
-# cc -c lex.yy.c y.tab.c
-# cc -o puf_g.out lex.yy.o y.tab.o -ll
-#  ./puf_g.out puf_designs/test_xor_ipuf.pufg 
+echo "---------------------------------------------------------------" 
+echo "                           XOR-IPUF"
+echo "---------------------------------------------------------------" 
+lex puf_g.l
+yacc -d puf_g.y
+cc -c lex.yy.c y.tab.c
+cc -o puf_g.out lex.yy.o y.tab.o -ll
+ ./puf_g.out puf_designs/test_xor_ipuf.pufg 
 
 # echo "---------------------------------------------------------------" 
 # echo "                           XOR-CASCADED-IPUF"
